823c6fea4b6dd3e8a8af0d3b5329dd8cf393b4fa
yzy1997 <2032165508@qq.com>
2024-06-12 10:14:20 +0300

    add timestamp conversion

---
+++ src/timestamp_base_conversion/unix2norm_base.py
@@ -0,0 +1,10 @@
+import datetime
+
+# Given timestamp
+timestamp = 1718175966.6030958
+
+# Convert the timestamp to a datetime object
+date_time = datetime.datetime.fromtimestamp(timestamp)
+
+# Print the result
+print(date_time)
